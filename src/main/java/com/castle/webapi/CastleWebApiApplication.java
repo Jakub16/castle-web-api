e084028751cced1758271fe7f3a7ec03796da306
kubaz <jakub.zawadzki16@gmail.com>
2023-01-23 21:45:09 +0100

    initial commit

---
+++ src/main/java/com/castle/webapi/CastleWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.castle.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castle/webapi/CastleWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castle.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
