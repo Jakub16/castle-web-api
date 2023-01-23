53ff037364d44363f43729fdc443b45738d14936
kubaz <jakub.zawadzki16@gmail.com>
2023-01-23 21:45:59 +0100

    Created and connected castle-web-api module

--- src/main/java/com/castle/webapi/CastleWebApiApplication.java
+++ src/main/java/com/castle/webapi/CastleWebApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CastleWebApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CastleWebApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CastleWebApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/castle/webapi/CastleWebApiApplicationTests.java
+++ src/test/java/com/castle/webapi/CastleWebApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CastleWebApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
